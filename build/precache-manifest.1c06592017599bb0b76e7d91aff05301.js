dda32d1f1cde9dc7961b8fa6ea5a44cc82cdb2ac
Esdras Pavon <pavonesdras@gmail.com>
2019-04-09 21:30:14 -0300

    url fixed

--- build/precache-manifest.1c06592017599bb0b76e7d91aff05301.js
+++ build/precache-manifest.1c06592017599bb0b76e7d91aff05301.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "dde7b92b383a66a03b8b",
-    "url": "/static/js/main.69d97e2c.chunk.js"
+    "revision": "a757460bb2a985151cd0",
+    "url": "/static/js/main.0bb39abd.chunk.js"
   },
   {
     "revision": "9eee67cf782a38d35aeb",
     "url": "/static/js/2.d6a534e5.chunk.js"
   },
   {
-    "revision": "dde7b92b383a66a03b8b",
+    "revision": "a757460bb2a985151cd0",
     "url": "/static/css/main.800a3723.chunk.css"
   },
   {
-    "revision": "6bfdac89927062f55e8c98bdbd72ce9d",
+    "revision": "9891ef4daf097b71121277f8a9f444eb",
     "url": "/index.html"
   }
 ];
